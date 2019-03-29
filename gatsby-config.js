836db5e635a41f47a2c0fcf630c5ba57d32d398a
Gianpaolo <gianpaolo@monterosa.co.uk>
2019-03-29 18:04:04 +0000

    start

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + WordPress Starter',
+    title: 'Nuovo Nuovo Website',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -9,16 +9,22 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: 'wpdemo.gatsbycentral.com',
+        baseUrl: 'gianpa.com',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
         // The protocol. This can be http or https.
-        protocol: 'https',
+        protocol: 'http',
         // Use 'Advanced Custom Fields' Wordpress plugin
-        useACF: false,
+        useACF: true,
         auth: {},
         // Set to true to debug endpoints on 'gatsby build'
         verboseOutput: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      options: {
+        // Add any options here
       },
     },
     'gatsby-plugin-sharp',
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/logo.webp'
 
 const Navbar = () => (
   <StaticQuery
@@ -23,7 +23,7 @@ const Navbar = () => (
           <div className="navbar-brand">
             <Link to="/" className="navbar-item">
               <figure className="image">
-                <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+                <img src={logo} alt="Nuovo Nuovo" style={{ width: '88px' }} />
               </figure>
             </Link>
           </div>
